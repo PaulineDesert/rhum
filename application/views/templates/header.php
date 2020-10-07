5904cdee2cec0307c0a6b7f183e1c0baf31101d9
JPestel <jerompestel@gmail.com>
2020-10-07 18:55:26 +0200

    add cookie and front-end

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,8 +7,8 @@
 
     <!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <!-- Bootswatch CSS -->
-		<!-- <link rel="stylesheet" href="https://bootswatch.com/4/solar/bootstrap.min.css"> -->
+    <!-- CSS AOS -->
+  	<link rel="stylesheet" href="assets/imports/AOS/css/aos.css" type="text/css" />
 
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
