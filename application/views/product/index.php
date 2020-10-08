ffc30f3933fb406c3588e632075c4fc4eae7d289
Noémie Macé <lhp3.mnoemie@gmail.com>
2020-10-08 11:42:14 +0200

    It work

--- application/views/product/index.php
+++ application/views/product/index.php
@@ -1,12 +1,16 @@
 <h2><?= $title; ?></h2>
-
-<?php foreach ($product as $products_item): ?>
-	<div class="card" style="width: 18rem;">
-		<img src="<?= $products_item['product_image']; ?>" class="card-img-top" alt="...">
-		<div class="card-body">
-			<h5 class="card-title"><?= $products_item['product_name']; ?></h5>
-			<p class="card-text"><?= $products_item['product_description']; ?></p>
-			<a href="<?= site_url('product/'.$products_item['product_id']); ?>/" class="btn btn-primary">Ajouter au panier</a>
+<div class="row">
+	<?php foreach ($product as $products_item): ?>
+	<div class="col-4">
+		<div class="card" style="width: 18rem;">
+			<img src="<?= $products_item['product_image']; ?>" class="card-img-top" alt="...">
+			<div class="card-body">
+				<h5 class="card-title"><?= $products_item['product_name']; ?></h5>
+				<p class="card-text"><?= $products_item['product_description']; ?></p>
+				<a href="<?= site_url('product/'.$products_item['product_id']); ?>/" class="btn btn-primary">Ajouter au panier</a>
+			</div>
 		</div>
 	</div>
-<?php endforeach; ?>
+	<?php endforeach; ?>
+</div>
+
