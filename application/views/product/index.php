efa236cc24ee11fee5ba6a2d98d026cd966c0398
Noémie Macé <lhp3.mnoemie@gmail.com>
2020-10-08 11:33:42 +0200

    ON A DES PRODUITS QUI FONCTIONNENT ENFIN !

--- application/views/product/index.php
+++ application/views/product/index.php
@@ -1,12 +1,12 @@
-<h2><?php echo $title; ?></h2>
+<h2><?= $title; ?></h2>
 
-<?php foreach ($products as $products_item): ?>
+<?php foreach ($product as $products_item): ?>
 	<div class="card" style="width: 18rem;">
-		<img src="<?php echo $products_item['product_image']; ?>" class="card-img-top" alt="...">
+		<img src="<?= $products_item['product_image']; ?>" class="card-img-top" alt="...">
 		<div class="card-body">
-			<h5 class="card-title"><?php echo $products_item['product_name']; ?></h5>
-			<p class="card-text"><?php echo $products_item['product_description']; ?></p>
-			<a href="<?php echo site_url('index.php/admin/'.$products_item['product_id']); ?>" class="btn btn-primary">Ajouter au panier</a>
+			<h5 class="card-title"><?= $products_item['product_name']; ?></h5>
+			<p class="card-text"><?= $products_item['product_description']; ?></p>
+			<a href="<?= site_url('product/'.$products_item['product_id']); ?>/" class="btn btn-primary">Ajouter au panier</a>
 		</div>
 	</div>
 <?php endforeach; ?>
