e5dd8f9b1ffa6b3232cc445f11d6b1b18efacc27
Noémie Macé <lhp3.mnoemie@gmail.com>
2020-10-08 15:51:22 +0200

    Inscription des clients

--- application/config/routes.php
+++ application/config/routes.php
@@ -58,11 +58,16 @@ $route['admin/create'] = 'admin/create';
 $route['admin/(:any)'] = 'admin/view/$1';
 $route['admin'] = 'admin';
 
+// Route customer
+$route['customer/create'] = 'customer/create';
+$route['customer/(:any)'] = 'customer/view/$1';
+$route['customer'] = 'customer';
+
 // Route par défaut
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
 
-//Route pour register
-$route['register'] = 'register';
+// //Route pour register
+// $route['register'] = 'register';
 
 
